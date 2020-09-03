9968a3601180863dfc8dceaedeedc96d4cbbfbe8
Francis <francisconlm@hotmail.com>
2020-09-03 15:18:51 +0100

    Add files via upload

--- mult/mult.go
+++ mult/mult.go
@@ -1,6 +1,6 @@
 package mult
 
-// Add takes two integers and returns the sum of them.
+// Takes two integers and returns the multiplication of them.
 func Two(x, y int) int {
     return x * y
 }
--- sub/sub.go
+++ sub/sub.go
@@ -1,6 +1,6 @@
 package sub
 
-// Subtracts takes two integers and returns the sum of them.
+// Takes two integers and returns the subtraction of them.
 func Two(x, y int) int {
     return x - y
 }
